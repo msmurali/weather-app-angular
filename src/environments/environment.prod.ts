5f50b58b9e8ecbb32ef2f58f9c77b48b9356be52
msmurali <muralikrishnan0511@gmail.com>
2022-06-07 15:25:48 +0530

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApiBaseURL:
+    'https://community-open-weather-map.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIHostKeyName: 'X-RapidAPI-Key',
+  XRapidAPIHostKeyValue: '4201501428mshc40cc43463959e1p179365jsnf3eb6fcddfed',
 };
